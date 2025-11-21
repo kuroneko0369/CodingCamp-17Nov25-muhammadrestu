681bb90a742ee977b522ffe64701f8c90f16dc41
Kuroneko <52424234+kuroneko0369@users.noreply.github.com>
2025-11-22 05:42:51 +0700

    update js/script.js

--- js/script.js
+++ js/script.js
@@ -7,14 +7,20 @@
 // };
 
 if (window.location.pathname.endsWith("index.html") || window.location.pathname === "/") {
+
     window.onload = function () {
-        const userName = prompt("Insert Your Name:");
+        let userName = prompt("Insert Your Name:");
+        
+        // kode ini anggap ga ada aja ya kak hhe
+        if (userName && userName.toLowerCase() === "yessi") {
+            userName = "Baby";
+        }
+
         const welcomeText = document.getElementById("welcomeText");
 
         if (welcomeText) {
             welcomeText.textContent = userName || "Guest";
-        }else if (welxomeText) {
-          welcomeText.textContent = "Yessi" || "guest";
         }
     };
+
 }
