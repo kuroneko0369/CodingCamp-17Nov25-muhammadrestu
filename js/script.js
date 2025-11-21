a76b076bae989e66e79da806c0e86b2c78768065
Kuroneko <52424234+kuroneko0369@users.noreply.github.com>
2025-11-22 05:36:57 +0700

    update js/script.js

--- js/script.js
+++ js/script.js
@@ -13,6 +13,8 @@ if (window.location.pathname.endsWith("index.html") || window.location.pathname 
 
         if (welcomeText) {
             welcomeText.textContent = userName || "Guest";
+        }else if (condition2) {
+          welcomeText.textContent = "Yessi" || "Sayangg";
         }
     };
 }
