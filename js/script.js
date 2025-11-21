182e79bfbd04ce535c40dda7bea61510909f62e5
Kuroneko <52424234+kuroneko0369@users.noreply.github.com>
2025-11-22 03:43:23 +0700

    update js/script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 if (window.location.pathname.includes("index.html") || window.location.pathname === "/") {
     let username = prompt("Masukkan nama kamu:");
 
-    if (username && username.trim() !== "") {
-        document.getElementById("welcomeText").innerText = `Hi ${username}, Selamat Datang Di Website`;
+    if (username && username.trim() !== "yessi") {
+        document.getElementById("welcomeText").innerText = `Halo babyy, Selamat Datang Di Website Aku`;
+    }
+    else (username && username.trim() !== "") {
+        document.getElementById("welcomeText").innerText = `Halo ${username}, Selamat Datang Di Website`;
     }
 }
