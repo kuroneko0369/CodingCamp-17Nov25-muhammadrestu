149ad1e4d2eef8c604e88d1aad48ec47240f57b3
Kuroneko <52424234+kuroneko0369@users.noreply.github.com>
2025-11-22 05:18:46 +0700

    update js/script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-if (window.location.pathname.includes("index.html") || window.location.pathname === "/") {
-    let username = prompt("Masukkan nama kamu:");
+// if (window.location.pathname.includes("index.html") || window.location.pathname === "/") {
+//     let username = prompt("Masukkan nama kamu:");
 
-    if (username && username.trim() !== "yessi") {
-        document.getElementById("welcomeText").innerText = `Halo babyy, Selamat Datang Di Website Aku`;
-    }
-    else (username && username.trim() !== "") {
-        document.getElementById("welcomeText").innerText = `Halo ${username}, Selamat Datang Di Website`;
-    }
-}
+//     if (username && username.trim() !== "yessi") {
+//         document.getElementById("welcomeText").innerText = `Halo babyy, Selamat Datang Di Website Aku`;
+//     }
+//     else (username && username.trim() !== "") {
+//         document.getElementById("welcomeText").innerText = `Halo ${username}, Selamat Datang Di Website`;
+//     }
+// }
+// Welcome popup jadul tapi manjur
+window.onload = function () {
+    const userName = prompt("Masukkan nama kamu:");
+    const welcomeText = document.getElementById("welcome-name");
+
+    // Kalau user cancel / kosong → fallback
+    welcomeText.textContent = userName || "Tamu";
+};
